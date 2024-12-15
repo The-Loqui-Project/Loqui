3208f406d0016c2d3e483a8f985b0b5d7e3ac663
Daniel (aka. rotgruengelb) <rotgrungelblab@gmail.com>
2024-12-15 18:02:20 +0100

    feat(monorepo): turboprop fastify setup

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -1,5 +1,5 @@
 import cors from '@fastify/cors'
-import fastify, { FastifyInstance } from 'fastify'
+import fastify, {FastifyInstance} from 'fastify'
 import routes from './routes';
 import APIRoute from './routes/route';
 
@@ -12,26 +12,24 @@ server.register(cors);
 const apiVersions = Object.keys(routes);
 
 for (const apiVersion of apiVersions) {
-  const routeGroup = routes[apiVersion];
-  for (const routeName of Object.keys(routeGroup)) {
-    const routeURL = `/api/${apiVersion}/${routeName}`;
-    const routeObj: APIRoute = routeGroup[routeName];
+    const routeGroup = routes[apiVersion];
+    for (const routeName of Object.keys(routeGroup)) {
+        const routeURL = `/api/${apiVersion}/${routeName}`;
+        const routeObj: APIRoute = routeGroup[routeName];
 
-    server.route({
-      method: routeObj.type,
-      url: routeURL,
-      handler: routeObj.func,
-    });
+        server.route({
+            method: routeObj.type, url: routeURL, handler: routeObj.func,
+        });
 
-    server.log.info(`Registered route: ${routeURL}`);
-  }
+        server.log.info(`Registered route: ${routeURL}`);
+    }
 }
 
 // Start the server
-server.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    server.log.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
+server.listen({port: 8080}, (err, address) => {
+    if (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
 });
