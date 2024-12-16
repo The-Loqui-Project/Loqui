2261c7cb3ca6e9a170136523ab6a87afb363339f
Calum <hendersoncal117@gmail.com>
2024-12-16 17:59:05 +0000

    Swagger

--- apps/api/routes/route.ts
+++ apps/api/routes/route.ts
@@ -2,5 +2,6 @@ import { FastifyRequest, FastifyReply } from "fastify";
 
 export default interface APIRoute {
     type: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'OPTIONS' | 'HEAD';
+    schema;
     func: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
   }
--- apps/api/server.ts
+++ apps/api/server.ts
@@ -1,12 +1,57 @@
 import cors from '@fastify/cors'
-import fastify, {FastifyInstance} from 'fastify'
+import fastify, { FastifyInstance } from 'fastify'
 import routes from './routes';
+import swagger from "@fastify/swagger"
 import APIRoute from './routes/route';
 
 const server: FastifyInstance = fastify();
 
 // Register CORS
 server.register(cors);
+
+if (!process.env.PRODUCTION) {
+    server.register(swagger, {
+        openapi: {
+            openapi: '3.0.0',
+            info: {
+                title: 'Loqui API',
+                description: 'Documentation on Loqui API routes.',
+                version: 'v1'
+            },
+            servers: [
+                {
+                    url: 'http://localhost:8080',
+                    description: 'Development server'
+                }
+            ],
+            components: {
+                securitySchemes: {
+                    modrinthToken: {
+                        type: 'apiKey',
+                        name: 'modrinthToken',
+                        in: 'header'
+                    }
+                }
+            }
+        }
+    });
+
+    server.register(import('@fastify/swagger-ui'), {
+        routePrefix: '/documentation',
+        uiConfig: {
+          docExpansion: 'full',
+          deepLinking: false
+        },
+        uiHooks: {
+          onRequest: function (request, reply, next) { next() },
+          preHandler: function (request, reply, next) { next() }
+        },
+        staticCSP: true,
+        transformStaticCSP: (header) => header,
+        transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
+        transformSpecificationClone: true
+    });
+}
 
 // Register routes dynamically
 for (const [apiVersion, routeGroup] of Object.entries(routes)) {
@@ -23,11 +68,21 @@ for (const [apiVersion, routeGroup] of Object.entries(routes)) {
     }
 }
 
-// Start the server
-server.listen({port: 8080}, (err, address) => {
-    if (err) {
-        server.log.error(err);
-        process.exit(1);
+(async () => {
+    await server.ready();
+    
+    if (!process.env.PRODUCTION) {
+        server.swagger();
+        console.log("Swagger available at http://localhost:8080/documentation")
     }
-    console.log(`Server listening at ${address}`);
-});
+
+    // Start the server
+    server.listen({ port: 8080 }, (err, address) => {
+        if (err) {
+            server.log.error(err);
+            process.exit(1);
+        }
+        console.log(`Server listening at http://localhost:8080/`);
+    });
+})();
+
