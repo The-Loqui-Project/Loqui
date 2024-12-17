f2b6c3cbd88788fb78bc73055aa15b4186184d25
Calum H. <hendersoncal117@gmail.com>
2024-12-17 14:13:44 +0000

    feat(api/oauth): Oauth process implementation. (#2)

--- apps/api/db/index.ts
+++ apps/api/db/index.ts
@@ -1,9 +1,9 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+
 import { schema } from "./schema/schema";
-import "dotenv/config";
 
 const db = drizzle(process.env.DATABASE_URL!, {
-  schema: schema,
+	schema: schema,
 });
 
 export default db;
