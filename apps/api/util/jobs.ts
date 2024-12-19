67207c07d00f4470e262eb852fe449e8e4b89513
Calum H. <hendersoncal117@gmail.com>
2024-12-19 22:22:54 +0000

    feat(api): Graceful shutdown.

--- apps/api/util/jobs.ts
+++ apps/api/util/jobs.ts
@@ -1,1 +1,26 @@
-export default async function setupJobs() {}
+export async function setupJobs() {}
+
+let isShuttingDown: boolean = false;
+export async function gracefulShutdown(signal: string, server: any) {
+  if (isShuttingDown) {
+    return;
+  }
+  isShuttingDown = true;
+  console.info(`${signal} received. Gracefully shutting down.`);
+
+  // Wait 30 seconds for existing connections to finish
+  const timeout = setTimeout(() => {
+    console.warn("Forcefully shutting down after 20 seconds.");
+    process.exit(1); // Exit with a non-zero code to indicate an issue on shutdown
+  }, 10000);
+
+  timeout.unref();
+
+  // Stop accepting new connections
+  await server.close();
+
+  console.log("Closed out remaining connections.");
+  clearTimeout(timeout);
+
+  process.exit(0);
+}
