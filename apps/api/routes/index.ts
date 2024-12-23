46dc3ad63cb7c2a86d2eef49ddd879b1da185bf8
Calum H. <hendersoncal117@gmail.com>
2024-12-23 21:51:22 +0000

    feat(api): add endpoint to retrieve details of a single string in a project

--- apps/api/routes/index.ts
+++ apps/api/routes/index.ts
@@ -8,6 +8,7 @@ import V1_OAuthConfiguration from "./v1/oauth/configuration";
 // Projects
 import V1_ProjectManagementSubmit from "./v1/projects/management/submit";
 import V1_ProjectGetStrings from "./v1/projects/[id]/strings";
+import V1_ProjectGetString from "./v1/projects/[id]/string/[id]";
 
 interface RouteStorage {
   [apiVersion: string]: {
@@ -21,6 +22,7 @@ const routes: RouteStorage = {
     oauth_configuration: V1_OAuthConfiguration,
     projects_management_submit: V1_ProjectManagementSubmit,
     projects_get_strings: V1_ProjectGetStrings,
+    projects_get_string: V1_ProjectGetString,
   },
 };
 
