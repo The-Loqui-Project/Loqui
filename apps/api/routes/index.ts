d6f8a501c49f7d8db929d041222aa056ce7f5d95
Calum H. <hendersoncal117@gmail.com>
2024-12-24 20:42:13 +0000

    feat(api): add report endpoint for projects

--- apps/api/routes/index.ts
+++ apps/api/routes/index.ts
@@ -9,6 +9,7 @@ import V1_OAuthConfiguration from "./v1/oauth/configuration";
 import V1_ProjectManagementSubmit from "./v1/projects/management/submit";
 import V1_ProjectGetStrings from "./v1/projects/[id]/strings";
 import V1_ProjectGetString from "./v1/projects/[id]/string/[id]";
+import V1_ProjectReport from "./v1/projects/[id]/report";
 
 interface RouteStorage {
   [apiVersion: string]: {
@@ -18,11 +19,15 @@ interface RouteStorage {
 
 const routes: RouteStorage = {
   v1: {
+    // Authentication
     oauth_finalize: V1_OAuthFinalize,
     oauth_configuration: V1_OAuthConfiguration,
-    projects_management_submit: V1_ProjectManagementSubmit,
-    projects_get_strings: V1_ProjectGetStrings,
-    projects_get_string: V1_ProjectGetString,
+
+    // Projects
+    project_management_submit: V1_ProjectManagementSubmit,
+    project_get_strings: V1_ProjectGetStrings,
+    project_get_string: V1_ProjectGetString,
+    project_report: V1_ProjectReport,
   },
 };
 
