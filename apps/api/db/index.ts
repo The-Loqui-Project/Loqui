38f81fd232050f80c3ea29b7a30b6e32cf40a267
Steveplays28 <djspaargaren@outlook.com>
2025-04-14 17:32:46 +0200

    fix: :bug: Work around a type issue with the PGlite database

--- apps/api/db/index.ts
+++ apps/api/db/index.ts
@@ -1,14 +1,14 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
 import { drizzle as drizzlePgLite } from "drizzle-orm/pglite";
 
 import { schema } from "./schema/schema";
 
 const db =
   process.env.DEV_MODE && process.env.USE_PGLITE_DATABASE_CONNECTION
-    ? drizzlePgLite("./dist/pglite_database", {
+    ? (drizzlePgLite("./dist/pglite_database", {
         schema: schema,
-      })
+      }) as unknown as NodePgDatabase<typeof schema>)
     : drizzle(process.env.DATABASE_URL!, {
         schema: schema,
       });
