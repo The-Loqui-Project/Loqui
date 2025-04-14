424e6a08f6aaf7a37c5423fa2dbff07d16cf33bb
Steveplays28 <Djspaargaren@outlook.com>
2025-04-14 18:46:39 +0200

    Add PGlite support in development environments (#34)
    Co-authored-by: Calum H. <hendersoncal117@gmail.com>

--- apps/api/db/index.ts
+++ apps/api/db/index.ts
@@ -1,10 +1,16 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle, NodePgDatabase } from "drizzle-orm/node-postgres";
+import { drizzle as drizzlePgLite } from "drizzle-orm/pglite";
 
 import { schema } from "./schema/schema";
 
-const db = drizzle(process.env.DATABASE_URL!, {
-  schema: schema,
-});
+const db =
+  process.env.DEV_MODE && process.env.USE_PGLITE_DATABASE_CONNECTION
+    ? (drizzlePgLite("./dist/pglite_database", {
+        schema: schema,
+      }) as unknown as NodePgDatabase<typeof schema>)
+    : drizzle(process.env.DATABASE_URL!, {
+        schema: schema,
+      });
 
 export default db;
