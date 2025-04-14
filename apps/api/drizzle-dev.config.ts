2175263363092ef6e00ce044d0d30053f121e90a
Steveplays28 <djspaargaren@outlook.com>
2025-04-14 16:21:14 +0200

    feat: :sparkles: Add PGlite support in development environments

--- apps/api/drizzle-dev.config.ts
+++ apps/api/drizzle-dev.config.ts
@@ -1,11 +1,23 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-export default defineConfig({
-  out: "./db/drizzle",
-  schema: "./db/schema/schema.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: "postgres://postgres:loqui_test_db-password@localhost:3984/postgres",
-  },
-});
+export default defineConfig(
+  process.env.DEV_MODE && process.env.USE_PGLITE_DATABASE_CONNECTION
+    ? {
+        out: "./db/drizzle",
+        driver: "pglite",
+        schema: "./db/schema/schema.ts",
+        dialect: "postgresql",
+        dbCredentials: {
+          url: "./dist/pglite_database",
+        },
+      }
+    : {
+        out: "./db/drizzle",
+        schema: "./db/schema/schema.ts",
+        dialect: "postgresql",
+        dbCredentials: {
+          url: "postgres://postgres:loqui_test_db-password@localhost:3984/postgres",
+        },
+      },
+);
