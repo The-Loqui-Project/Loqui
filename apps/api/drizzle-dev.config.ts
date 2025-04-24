e02abe368a01b7f683c5fde7fb9af491096c29ed
Calum H. <hendersoncal117@gmail.com>
2025-04-24 14:50:12 +0100

    fix: remove pglite

--- apps/api/drizzle-dev.config.ts
+++ apps/api/drizzle-dev.config.ts
@@ -2,23 +2,12 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig(
-  process.env.DEV_MODE === "true" &&
-    process.env.USE_PGLITE_DATABASE_CONNECTION === "true"
-    ? {
-        out: "./db/drizzle",
-        driver: "pglite",
-        schema: "./db/schema/schema.ts",
-        dialect: "postgresql",
-        dbCredentials: {
-          url: "./dist/pglite_database",
-        },
-      }
-    : {
+  {
         out: "./db/drizzle",
         schema: "./db/schema/schema.ts",
         dialect: "postgresql",
         dbCredentials: {
           url: process.env.DATABASE_URL!,
         },
-      },
+  }
 );
