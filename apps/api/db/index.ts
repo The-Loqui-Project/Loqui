c98974acc5b05594c9ae2224a0ac36ed61b364fb
Calum H. <hendersoncal117@gmail.com>
2025-04-24 14:33:18 +0100

    fix: Temporarily remove pglite.

--- apps/api/db/index.ts
+++ apps/api/db/index.ts
@@ -4,8 +4,8 @@ import { drizzle as drizzlePgLite } from "drizzle-orm/pglite";
 
 import { schema } from "./schema/schema";
 
-const db =
-  process.env.DEV_MODE === "true" &&
+/**
+process.env.DEV_MODE === "true" &&
   process.env.USE_PGLITE_DATABASE_CONNECTION === "true"
     ? (drizzlePgLite("./dist/pglite_database", {
         schema: schema,
@@ -13,5 +13,10 @@ const db =
     : drizzle(process.env.DATABASE_URL!, {
         schema: schema,
       });
+**/
+
+const db = drizzle(process.env.DATABASE_URL!, {
+  schema: schema
+});
 
 export default db;
